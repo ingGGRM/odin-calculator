5834c25138130b16f98c5847af3be2350750bc41
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-06-02 09:03:49 -0400

    Added js function to generate calculator buttons. Also added init function called from html body after page is first loaded

---
+++ script.js
@@ -0,0 +1,29 @@
+const screen = document.getElementById("screen");
+const numButtons = document.getElementById("num-buttons");
+const funcButtons = document.getElementById("func-buttons");
+
+// const arrays for each button label
+const numbers = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0', '=']
+const functions = ['/', 'X', '-', '+']
+
+/* init function called on page load */
+function init() {
+    genCalculator();
+}
+
+/* function that generate all calculator buttons */
+function genCalculator() {
+    for(let i = 0; i < 12; i++) { // 11 button element creation for 0-9, dot(.) and equal(=)
+        const numButton = document.createElement('button');
+        numButton.textContent = numbers[i];
+        numButton.classList.add('button');
+        numButtons.appendChild(numButton);
+    }
+
+    for(let i = 0; i < functions.length; i++) { // 4 button element creation for math functions (+, -, *, /)
+        const funcButton = document.createElement('button');
+        funcButton.textContent = functions[i];
+        funcButton.classList.add('button');
+        funcButtons.appendChild(funcButton);
+    }
+}
