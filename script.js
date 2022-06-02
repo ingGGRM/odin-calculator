0b353052eb172fc91950fb06865ce3d649a3cbd3
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-06-02 10:37:57 -0400

    Changed buttons location putting zero as a big two column button. Added AC button and equal button now is under the plus sign button Zero button now have two classes (button and zero)

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const numButtons = document.getElementById("num-buttons");
 const funcButtons = document.getElementById("func-buttons");
 
 // const arrays for each button label
-const numbers = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0', '=']
-const functions = ['/', 'X', '-', '+']
+const numbers = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0']
+const functions = ['AC', '/', 'X', '-', '+', '=']
 
 /* init function called on page load */
 function init() {
@@ -13,10 +13,11 @@ function init() {
 
 /* function that generate all calculator buttons */
 function genCalculator() {
-    for(let i = 0; i < 12; i++) { // 11 button element creation for 0-9, dot(.) and equal(=)
+    for(let i = 0; i < numbers.length; i++) { // 11 button element creation for 0-9, dot(.) and equal(=)
         const numButton = document.createElement('button');
         numButton.textContent = numbers[i];
         numButton.classList.add('button');
+        if(numbers[i] == '0') numButton.classList.add('zero');
         numButtons.appendChild(numButton);
         numButton.addEventListener('click', buttonClick);
     }
@@ -26,6 +27,7 @@ function genCalculator() {
         funcButton.textContent = functions[i];
         funcButton.classList.add('button');
         funcButtons.appendChild(funcButton);
+        funcButton.addEventListener('click', buttonClick);
     }
 }
 
